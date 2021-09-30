524f2118bb00802b7d611d21905765fc5f73cffb
t-nakamura <1am1goodtime@gmail.com>
2021-09-30 07:51:47 +0000

    practice

--- pra2.rb
+++ pra2.rb
@@ -1,1 +1,27 @@
-print("Hello, \"Ruby\".\n")
+tango = ["knowledge", "information", "system", "library", "metadata"]
+
+while true 
+  puts "単語を入力してください"
+  puts "終わる時は . (ピリオド)を入力してください"
+  a = gets.chomp
+  if a== "."
+    break
+  end
+  
+  j = 0
+  while j < tango.size
+    if a == tango[j]
+      puts a + "は辞書に登録されています"
+      break
+    end
+  j = j+1
+  end
+  if j == tango.size
+    puts a + "を登録します"
+    tango.push(a)
+  end
+end
+
+tango.each{|value|
+  puts value
+}
